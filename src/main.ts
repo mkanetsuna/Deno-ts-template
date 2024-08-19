d4398b2d2158efddcab690774854e8e4c8862f18
masaaya <156692516+mkanetsuna@users.noreply.github.com>
2024-08-19 06:51:27 +0000

    successed!

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v16.1.0/mod.ts";
 import { config } from "./config.ts";
 
 const app = new Application();
